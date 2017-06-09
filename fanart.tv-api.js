d60b312f35b581f3cb17c0e9c22621888f4a7def
Chris Alderson <chrisalderson@protonmail.com>
2017-06-09 17:41:26 +0200

    refactor with got

--- fanart.tv-api.js
+++ fanart.tv-api.js
@@ -1,86 +1,56 @@
 'use strict';
 
 const querystring = require('querystring');
-const request = require('request');
-
-const defaultOptions = {
-  baseUrl: 'http://webservice.fanart.tv/v3/',
-  timeout: 5 * 1000
-};
+const got = require('got');
 
 module.exports = class FanartTVAPI {
 
-  constructor({api_key, options = defaultOptions, debug = false} = {}) {
-    this._request = request.defaults(options);
+  constructor({api_key, baseUrl = 'http://webservice.fanart.tv/v3/', debug = false} = {}) {
+    this._baseUrl = baseUrl;
     this._debug = debug;
 
     if (!api_key) throw new Error(`No API key given!`);
     FanartTVAPI.api_key = api_key;
   }
 
-  _get(uri, qs, retry = true) {
-    if (this._debug) console.warn(`Making request to uri: ${uri}, qs: '${querystring.stringify(qs)}'`);
-    return new Promise((resolve, reject) => {
-      return this._request({ uri, qs }, (err, res, body) => {
-        if (err && retry) {
-          return resolve(this._get(uri, qs, false));
-        } else if (err) {
-          return reject(err);
-        } else if (!body || res.statusCode >= 400) {
-          return reject(new Error(`No data found for uri: ${uri}, qs: '${querystring.stringify(qs)}', statuscode: ${res.statusCode}`))
-        } else {
-          return resolve(JSON.parse(body));
-        }
-      });
-    });
+  _get(uri, query = {api_key: FanartTVAPI.api_key}) {
+    if (this._debug)
+      console.warn(`Making request to: '${uri}${querystring.stringify(query)}`);
+
+    return got.get(`${this._baseUrl}/${uri}`, { query })
+      .then(({body}) => body);
   }
 
   getMovieImages(id) {
-    return this._get(`movies/${id}`, {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get(`movies/${id}`);
   }
 
   getLatestMoviesImages() {
-    return this._get('movies/latest', {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get('movies/latest');
   }
 
   getShowImages(id) {
-    return this._get(`tv/${id}`, {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get(`tv/${id}`);
   }
 
   getLatestShowsImages() {
-    return this._get('tv/latest', {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get('tv/latest');
   }
 
   getArtistImages(id) {
-    return this._get(`music/${id}`, {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get(`music/${id}`);
   }
 
   getAlbumImages(id) {
-    return this._get(`music/albums/${id}`, {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get(`music/albums/${id}`);
   }
 
   getLabelImages(id) {
-    return this._get(`music/labels/${id}`, {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get(`music/labels/${id}`);
   }
 
   getLatestArtistsImages() {
-    return this._get('music/latest', {
-      api_key: FanartTVAPI.api_key
-    });
+    return this._get('music/latest');
   }
 
 }
